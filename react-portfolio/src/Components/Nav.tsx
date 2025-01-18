485648e18a779ace54d5a83736b2802eb5fe5085
Dawson Adams <dawson.adams121@gmail.com>
2025-01-18 04:55:02 -0600

    Resume Page
    Created Resume Page with embedded pdf document.

--- react-portfolio/src/Components/Nav.tsx
+++ react-portfolio/src/Components/Nav.tsx
@@ -89,7 +89,7 @@ function Nav(props: {
             {mobileMenu}
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 lg:pl-7"
             id="navbar-sticky"
           >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
