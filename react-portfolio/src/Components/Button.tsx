1ef31faddeb845749a436fbff4a47ba423c837b5
Dawson Adams <dawson.adams121@gmail.com>
2025-03-02 21:04:47 -0600

    style(nav): current page button stands out
    Create a difference in opacity for nav buttons when that page is active.

--- react-portfolio/src/Components/Button.tsx
+++ react-portfolio/src/Components/Button.tsx
@@ -1,19 +1,25 @@
 interface Props {
   children: React.ReactNode;
+  currentPage?: boolean
 }
 
-function Button({ children }: Props) {
+function Button({ children, currentPage }: Props) {
   const top = () => {
     window.scrollTo({
       top: 0,
     });
   };
 
+  if (currentPage != undefined)
+    console.log(currentPage);
+
+  let style = currentPage ? "opacity-40" : "opacity-100";
+
   return (
     <>
       <button
-        className="transform hover:scale-[1.15] transition duration-200 ease-in-out relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-bold hover:text-light text-dark dark:text-light rounded-lg group 
-      bg-gradient-135 from-bright-orange from-20% to-regal-blue to-80% group-hover:from-bright-orange group-hover:to-regal-blue"
+        className={`${style} transform hover:scale-[1.15] transition duration-200 ease-in-out relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-sm font-bold hover:text-light text-dark dark:text-light rounded-lg group 
+      bg-gradient-135 from-bright-orange from-20% to-regal-blue to-80% group-hover:from-bright-orange group-hover:to-regal-blue`}
         onClick={top}
       >
         <span
