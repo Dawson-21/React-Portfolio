14ad0264075cc69c16779654262d064dacdeb50e
Dawson Adams <dawson.adams121@gmail.com>
2025-03-16 00:34:13 -0500

    fix(form): connect contact form to Vercel's serverless function

--- react-portfolio/src/components/ContactForm.tsx
+++ react-portfolio/src/components/ContactForm.tsx
@@ -1,109 +1,133 @@
-import React, { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
 
-interface ContactFormData {
-  email: string;
-  subject: string;
-  message: string;
-}
-
-const ContactForm: React.FC = () => {
-  const [formData, setFormData] = useState<ContactFormData>({
-    email: "",
-    subject: "",
-    message: "",
+export default () => {
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null as string | null },
   });
-  // const [status, setStatus] = useState("");
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    // Send formData to the backend
-    // setStatus("Sending...");
-
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      // setStatus("Message sent successfully!");
-      setFormData({ email: "", subject: "", message: "" });
-      console.log("Form submitted:", formData);
+  const [inputs, setInputs] = useState({
+    email: '',
+    message: '',
+  });
+  const handleServerResponse = (ok: boolean, msg: string) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      });
+      setInputs({
+        email: '',
+        message: '',
+      });
     } else {
-      // setStatus("An error occurred, please try again.");
-      console.log("Error submitting form:", formData);
+      setStatus({
+        submitted: false,
+        submitting: false,
+        info: { error: true, msg: msg },
+      });
     }
   };
-
+  const handleOnChange = (e: { persist: () => void; target: { id: any; value: any; }; }) => {
+    e.persist();
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+    setStatus({
+      submitted: false,
+      submitting: false,
+      info: { error: false, msg: null },
+    });
+  };
+  const handleOnSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+    axios({
+      method: 'POST',
+      url: 'https://formspree.io/f/mqapapol',
+      data: inputs,
+    })
+      .then((response) => {
+        handleServerResponse(
+          true,
+          'Thank you, your message has been submitted.',
+        );
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error);
+      });
+  };
   return (
-    <div className="2xl:px-[11vw] xl:px-[9vw] lg:px-[7vw] md:px-[15vw] sm:px-[10vw] px-[5vw] py-10 bg-transparent font-myFont">
-      <form onSubmit={handleSubmit}>
-        <div className="lg:w-1/2 w-full inline-block lg:pr-10 -translate-y-[17px]">
-          <label className="block lg:mb-4 text-lg font-medium text-dark dark:text-white">
-            Name
+    <>
+      
+      <div className="2xl:px-[11vw] xl:px-[9vw] lg:px-[7vw] md:px-[15vw] sm:px-[10vw] px-[5vw] py-10 bg-transparent font-myFont">
+        <form onSubmit={handleOnSubmit}>
+          <div className="lg:w-1/2 w-full inline-block lg:pr-10 -translate-y-[25px]">
+            <label htmlFor="name" className="block text-lg font-medium text-dark dark:text-white">Name</label>
             <input
               className="shadow-sm bg-white border border-dark text-dark text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-light dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              type="text"
-              placeholder="John Doe"
+              id="name"
+              type="name"
+              placeholder="your name"
               name="name"
-              value={formData.subject}
-              onChange={handleChange}
+              onChange={handleOnChange}
               required
+              value={inputs.email}
             />
-          </label>
-
-          <label className="block text-lg font-medium text-dark dark:text-white pt-[19px]">
-            Email
+            <label htmlFor="email" className="block text-lg font-medium text-dark dark:text-white pt-[27px]">Email</label>
             <input
               className="shadow-sm bg-white border border-dark text-dark text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-light dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              id="email"
               type="email"
-              placeholder="john.smith@email.com"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
+              placeholder="your email address"
+              name="_replyto"
+              onChange={handleOnChange}
               required
+              value={inputs.email}
             />
-          </label>
-        </div>
-
-        <div className="lg:w-1/2 w-full lg:inline-block">
-          <label className="block text-lg font-medium text-dark dark:text-white">
-            Message
+          </div>
+          <div className="lg:w-1/2 w-full lg:inline-block">
+            <label htmlFor="message" className="block text-lg font-medium text-dark dark:text-white">Message</label>
             <textarea
               id="message"
+              name="message"
               rows={5}
+              placeholder="Leave a message..."
               className="block p-2.5 w-full text-lg text-dark bg-white rounded-lg shadow-sm border border-dark focus:ring-primary-500 focus:border-primary-500 dark:bg-dark dark:border-gray-600 dark:placeholder-gray-400 dark:text-light dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a comment..."
-             />
-          </label>
-        </div>
+              onChange={handleOnChange}
+              required
+              value={inputs.message}
+            />
+          </div>
 
-        {/* <button type="submit">Send</button> 
-         Do I Need to include type="submit"? */}
-        <div className="lg:place-self-end place-self-center max-lg:pt-10">
-          <button type="submit"
-            className="transform hover:scale-[1.15] transition duration-200 ease-in-out relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-md font-bold hover:text-light text-dark dark:text-white rounded-lg group 
-            bg-gradient-135 from-bright-orange from-20% to-regal-blue to-80% group-hover:from-bright-orange group-hover:to-regal-blue"
-          >
-            <span
-              className="relative w-40 py-2.5 transition-all ease-in duration-200 
-          bg-white dark:bg-dark2 rounded-md group-hover:bg-opacity-0"
+          <div className="lg:place-self-end place-self-center max-lg:pt-10">
+            <button type="submit" disabled={status.submitting}
+              className="transform hover:scale-[1.15] transition duration-200 ease-in-out relative inline-flex items-center justify-center p-0.5 me-2 overflow-hidden text-md font-bold hover:text-light text-dark dark:text-white rounded-lg group 
+              bg-gradient-135 from-bright-orange from-20% to-regal-blue to-80% group-hover:from-bright-orange group-hover:to-regal-blue"
             >
-              Send Email
-            </span>
-          </button>
-        </div>
-      </form>
-    </div>
+              <span
+                className="relative w-40 py-2.5 transition-all ease-in duration-200 
+                bg-white dark:bg-dark2 rounded-md group-hover:bg-opacity-0"
+              >
+                {!status.submitting
+                ? !status.submitted
+                  ? 'Submit'
+                  : 'Submitted'
+                : 'Submitting...'}
+              </span>
+            </button>
+          </div>
+        </form>
+      </div>
+      
+      {status.info.error && (
+        <div className="error">Error: {status.info.msg}</div>
+      )}
+      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+
+    </>
   );
 };
-
-export default ContactForm;
