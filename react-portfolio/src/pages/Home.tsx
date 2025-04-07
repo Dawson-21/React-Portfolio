12c62bd22469e664f7192bc1d9b92db32e25ff0d
Dawson Adams <dawson.adams121@gmail.com>
2025-04-06 23:54:35 -0500

    fix(home): Updated home page layout

--- react-portfolio/src/pages/Home.tsx
+++ react-portfolio/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Button2 from "../components/Button2";
 import Card from "../components/Card";
 import SectionTitle from "../components/SectionTitle";
+import PieChart from "../components/PieChart";
 
 export default function Home() {
   return (
@@ -18,17 +19,15 @@ export default function Home() {
 
         <div className="grid max-w-screen-2xl px-4 py-8 mx-auto sm:px-8 xl:px-16 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 fade-in">
           <div className="mr-auto place-self-center lg:col-span-7">
-
             <h1 className="max-w-2xl mb-4 md:-mr-20 -mr-12 font-myFont font-bold tracking-tight leading-none [@media(max-width:450px)]:text-[26px] text-4xl sm:text-5xl lg:text-5xl xl:text-[55px] dark:text-light">
               Hi, I&#39;m Dawson Adams
             </h1>
 
             <p className="max-w-2xl mb-6 font-myFont text-[15px] sm:text-justify text-gray-600 lg:mb-8 xl:pr-20 lg:pr-10 text-lg dark:text-gray-400">
-              Professional web developer and React engineer with
-              skills ranging from front-end design and user experience
-              optimization to back-end development and database management. I
-              graduated from Texas A&M University - Corpus Christi with
-              a B.S. in Computer Science.
+              Professional web developer and React engineer with skills ranging
+              from front-end design and user experience optimization to back-end
+              development and database management. I graduated from Texas A&M
+              University - Corpus Christi with a B.S. in Computer Science.
             </p>
 
             <Link to="/about">
@@ -55,7 +54,6 @@ export default function Home() {
           <div className="hidden -z-10 lg:col-span-5 lg:flex xl:-ml-[27%] lg:-ml-[20%] lg:pt-0 lg:mt-16 md:-mb-16 xl:h-[33vw] lg:h-[30vw] xl:w-[50vw] lg:w-[45vw] fade-left">
             <img src="media/hero-img.png" alt="Dawson Adams" />
           </div>
-
         </div>
 
         <div
@@ -64,15 +62,13 @@ export default function Home() {
         >
           <div className="relative aspect-[1155/378] -translate-x-1/2 bg-gradient-to-r from-[#0047ff] to-[#ff4500] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" />
         </div>
-
       </div>
 
       {/****************** PROJECTS SECTION ******************/}
 
       <div className="md:pb-32 pb-20 md:pt-10 dark:bg-dark2 bg-light">
-
         <SectionTitle>MY LATEST PROJECTS</SectionTitle>
-                
+
         <div className="sm:inline-flex sm:flex-row flex-col xl:mx-[12%] lg:mx-[8%] md:mx-[5%] mx-[4%] lg:gap-5 md:gap-4 sm:gap-3 gap-2">
           <div className="md:max-w-[33%] rounded-xl shadow-xl dark:shadow-none bg-white dark:bg-dark dark:border-light border-white border-2">
             <Card
@@ -99,7 +95,7 @@ export default function Home() {
             />
           </div>
           <div className="block sm:hidden md:block md:max-w-[33%] sm:mt-0 mt-5 rounded-xl shadow-xl dark:shadow-none bg-white dark:bg-dark dark:border-light border-white border-2">
-                        <Card
+            <Card
               img="media/CNC2.png"
               alt="CNC Garage Doors Website"
               title="Local Business Website"
@@ -111,6 +107,53 @@ export default function Home() {
             />
           </div>
         </div>
+      </div>
+
+      {/****************** PIE CHART SECTION ******************/}
+
+      <PieChart />
+
+      {/****************** EDUCATION SECTION ******************/}
+
+      <div className="bg-light dark:bg-dark2">
+        <hr className="w-full h-[2px] mx-auto border-0 bg-gray-300" />
+
+        <SectionTitle>EDUCATION</SectionTitle>
+
+        <div className="grid max-w-[1400px] lg:grid-cols-2 lg:h-[400px] lg:px-0 md:px-20 px-10 md:pb-20 pb-10 pt-5 place-self-center">
+          <div className="lg:-mb-10 h-auto">
+            <img
+              src="media/tamucc.png"
+              alt="Texas A&M University - Corpus Christi Logo"
+              className="h-auto max-w-[600px] 2xl:w-[80%] xl:w-[80%] lg:w-[75%] sm:w-[40%] w-[50%] 
+                          xl:pl-[10%] xl:mx-0 lg:mx-auto lg:pr-0 pr-5 sm:ml-[27%] ml-[20%]"
+            />
+          </div>
+
+          <div className="font-myFont font-light dark:text-white leading-normal xl:text-lg lg:text-md text-base max-w-[770px] xl:-ml-[10%] lg:-ml-[9%] lg:mr-[10%] space-y-2 lg:mt-0 mt-5">
+            <p>
+              In December 2023, I earned my Bachelor&#39;s degree in Computer
+              Science with a specialization in Systems Programming from Texas
+              A&M University - Corpus Christi. During my time at the university,
+              I built a strong foundation in fundamental computer science
+              principles, including algorithms, data structures, and software
+              design, while exploring various fields within the industry. It was
+              during this time that I discovered my passion for coding and the
+              challenge of developing complex, scalable systems from the ground
+              up.
+            </p>
+            <p>
+              Since graduation, I’ve continued to deepen my expertise,
+              recognizing that the world of software development is vast and
+              ever-evolving. My commitment to lifelong learning drives me to
+              continuously expand my skill set, experimenting with new
+              programming languages and tools to stay current in this fast-paced
+              field.
+            </p>
+          </div>
+        </div>
+
+        <hr className="w-full h-[2px] mx-auto border-0 bg-gray-300 xl:mt-20 lg:mt-5" />
       </div>
     </>
   );
